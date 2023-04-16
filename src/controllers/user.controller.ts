6b70c577ae510d942a42afc333ace8aa08076e5d
tupis <joaoh.tupinamba@gmail.com>
2023-04-16 00:55:31 -0300

    feat: verifyToken; refac: use new throw error method in catch

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,23 +4,40 @@ import userService from "../services/user.service";
 const login = async (req: Request, res: Response) => {
   try {
     const user = await userService.login(req.body);
-    return res.status(200).send(user);
+    return res.status(200).json(user);
   } catch (error: any) {
-    console.log(error);
-    return res.status(400).send(error.message);
+    return res
+      .status(error.httpCode)
+      .json({ message: error.name, errorCode: error.httpCode });
   }
 };
 
 const register = async (req: Request, res: Response) => {
   try {
     const user = await userService.register(req.body);
-    return res.status(200).send(user);
+    return res.status(200).json(user);
   } catch (error: any) {
-    return res.status(400).send(error.message);
+    return res
+      .status(error.httpCode)
+      .json({ message: "deu erro", code: error.httpCode });
+  }
+};
+
+const verifyToken = async (req: Request, res: Response) => {
+  try {
+    const token = req?.headers?.authorization?.replace("Bearer ", "");
+
+    const verifyToken = await userService.verifyToken(token);
+    return res.status(200).json(verifyToken);
+  } catch (error: any) {
+    return res
+      .status(error.httpCode)
+      .json({ message: error.name, errorCode: error.httpCode });
   }
 };
 
 export default {
   login,
   register,
+  verifyToken,
 };

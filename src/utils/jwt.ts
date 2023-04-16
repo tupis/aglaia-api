46fefe7f84ee275c6c3eb14a5ec4ae55e906db09
tupis <joaoh.tupinamba@gmail.com>
2023-04-16 00:53:16 -0300

    feat: throw error

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,19 @@
 import { sign, verify } from "jsonwebtoken";
+import { AppError, HttpCode } from "../exceptions/appError";
 
 const secret = "mysecret";
 
 const verifyToken = (token: string) => {
-  return verify(token, secret);
+  return verify(token, secret, (err, decoded) => {
+    if (err) {
+      // throw new Error("Invalid token");
+      throw new AppError({
+        httpCode: HttpCode.UNAUTHORIZED,
+        description: "Invalid token",
+      });
+    }
+    return decoded;
+  });
 };
 
 const signToken = (payload: any) => {
